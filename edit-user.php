74d67e6aae988e098d4becbd3fd1b7eea14b8d7a
unknown <enockawor@gmail.com>
2025-01-16 09:02:14 -0800

    configured the database

--- edit-user.php
+++ edit-user.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>User Update</title>
-    <link rel="stylesheet" href="./css/bootstrap.min.css">
-</head>
-<body>
+<?php require_once('./includes/header.php') ?>
     <div class="container">
         <h2 class="pt-4">User Update</h2>
 
@@ -27,5 +18,4 @@
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
     </div>
-</body>
-</html>
+<?php require_once('./includes/footer.php') ?>
--- index.php
+++ index.php
@@ -1,14 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Task List</title>
-    <link rel="stylesheet" href="./css/bootstrap.min.css">
-</head>
-<body>
-    <div class="container">
+<?php require_once('./includes/header.php') ?>
+<div class="container">
         <form class="py-4">
             <div class="row">
                 <div class="col">
@@ -53,5 +44,4 @@
         </table>
 
     </div>
-</body>
-</html>
+<?php require_once('./includes/footer.php') ?>
